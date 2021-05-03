87bc2dd3f87958e136adc862c97eb69b7a0a6d8e
AnsonAMS <anson_tohata@hotmail.com>
2021-05-03 19:14:58 +0200

    Modify Searchbar.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -18,11 +18,14 @@ const business = {
 const businesses = [business, business, business, business, business, business];
 
 class App extends React.Component {
+  searchYelp(term, location, sortBy) {
+    console.log(`You are looking for ${term}, ${location}, and ${sortBy}`);
+  }
   render() {
     return (
       <div class="App">
         <h1>ravenous</h1>
-        <SearchBar />
+        <SearchBar searchYelp={this.searchYelp}/>
         <BusinessList businesses={businesses} />
       </div>
     );
